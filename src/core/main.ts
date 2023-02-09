922ea6178e4ec26ea0a7674014d094a077a588d4
SerhiiKrepets <7erhiii@gmail.com>
2023-02-09 14:03:42 +0200

    subFinalVersion

---
+++ src/core/main.ts
@@ -0,0 +1,25 @@
+// console.log(1);
+
+// document.querySelector('.themetoggle').addEventListener('click', (event) => {
+//    event.preventDefault();
+//    if (localStorage.getItem('theme') === 'dark') {
+//       localStorage.removeItem('theme');
+//    }
+//    else {
+//       localStorage.setItem('theme', 'dark')
+//    }
+//    addDarkClassToHTML()
+// });
+
+// function addDarkClassToHTML() {
+//    try {
+//       if (localStorage.getItem('theme') === 'dark') {
+//          document.querySelector('.page').classList.add('dark');
+//       }
+//       else {
+//          document.querySelector('.page').classList.remove('dark');
+//       }
+//    } catch (err) { }
+// }
+
+// addDarkClassToHTML();
